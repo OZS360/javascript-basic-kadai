ea23fa34638646772e7def5a4bd372ab13d44549
OZS <90957966+OZS360@users.noreply.github.com>
2024-11-17 14:38:03 +0900

    Delete event.js

--- event.js
+++
@@ -1,9 +0,0 @@
-const Btn = document.getElementById('btn');
-const h2 = document.getElementById('text');
-
-Btn.addEventListener('click', () => {
-  // li要素を新しく作成する
-
-　h2.textContent = 'ボタンをクリックしました。';
-
-});
